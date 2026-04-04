c38431a119bd5699f00df9d1c68af302a9cd4927
samueltuoyo15 <samueltuoyo9082@gmail.com>
2026-04-04 21:32:03 +0100

    feat(app): redirect root path to create meet page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
-import Main from './pages/Main';
 import CreateMeet from './pages/CreateMeet';
 import Room from './pages/Room';
 import NotFoundPage from './pages/NotFoundPage';
@@ -10,10 +8,10 @@ function App() {
     <>
      <Router>
       <Routes>
-     <Route path="/" element={<Main />}/>
-     <Route path="/meet/new" element={<CreateMeet />} />
-     <Route path="/meet/room/:roomId" element={<Room />}/>
-     <Route path="*" element={<NotFoundPage />} />
+        <Route path="/" element={<Navigate to="/meet/new" replace />} />
+        <Route path="/meet/new" element={<CreateMeet />} />
+        <Route path="/meet/room/:roomId" element={<Room />}/>
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
     </>
